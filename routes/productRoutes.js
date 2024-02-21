5133457ea065d81bbcc4bee7cd13d6aae263fd57
mohamedEdhehibi <mohamed.edhehibi@tekniva.net>
2024-02-21 15:35:32 +0100

    file uploads

---
+++ routes/productRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+  createProduct,
+  getAllProducts,
+} = require('../controllers/productController');
+const { uploadProductImageLocal } = require('../controllers/uploadsController');
+
+router.route('/').post(createProduct).get(getAllProducts);
+router.route('/uploads').post( uploadProductImageLocal);
+
+
+module.exports = router;
